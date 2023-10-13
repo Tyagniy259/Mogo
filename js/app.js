322d44832601149c80b9f38090d039d590e9a65f
sirena <147770721+Tyagniy259@users.noreply.github.com>
2023-10-13 17:51:40 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(function () {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrolloffset = $(window).scrollTop();
+    
+    //...Fixed header...//.................................................../
+    checkScroll(scrolloffset);
+
+    $(window).on("scroll", function () {
+
+        scrolloffset = $(this).scrollTop();
+        checkScroll(scrolloffset);
+
+    });
+
+    function checkScroll(scrolloffset) {
+        if (scrolloffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    //...Smooth scroll...//..................................................
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockid = $this.data('scroll'),
+            blockoffset = $(blockid).offset().top;
+
+            $("#nav a").removeClass("active")
+            $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockoffset
+        }, 500);
+    });
+
+
+    //...Menu nav toggle...//........................................................
+    $("#nav-toggle").on("click", function(event) {
+        event.preventDefault();
+ 
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    //...Collapse...//...............................................................
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockid = $this.data('collapse');
+
+            $(blockid).slideToggle(); 
+            $this.toggleClass('active');
+               
+    });
+
+
+    //...Slider...//............................................
+    $('.data-slider').slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+
+
+
+
+
+
+});
